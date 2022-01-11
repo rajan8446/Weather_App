6cba7a530ce21a263d09dea42c2cd1ecadaad021
Rajan Kumar <rajan8446@gmail.com>
2022-01-11 10:06:35 +0530

    first commit

---
+++ src/component/Weather.js
@@ -0,0 +1,69 @@
+import React,{useEffect,useState} from 'react'
+import WeatherInfo from './WeatherInfo';
+import './style.css'
+
+const Weather = () => {
+
+    const [searchValue, setSearchValue] = useState("");
+    const [tempInfo,setTempinfo]=useState({});
+
+    const getWeatherInfo=async()=>{
+        try{
+            let url=`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=d29eb7c6d67c180befce0c7c2d87c01e`;
+
+            let res=await fetch(url);
+            let data=await res.json();
+            const {temp,humidity,temp_max,temp_min,pressure} = data.main;
+            const {name}=data;
+            const {country,sunset}=data.sys;
+            const {speed} = data.wind;
+            const{main:weatherMood}=data.weather[0];
+
+            console.log(data)
+            console.log(weatherMood)
+
+            const myNewWeatherInfo = {
+                temp,
+                humidity,
+                temp_max,
+                temp_min,
+                pressure,
+                name,
+                country,
+                sunset,
+                speed,
+                weatherMood
+            }
+
+            setTempinfo(myNewWeatherInfo);
+        }
+        catch(error){
+            alert(error);
+        }
+    }
+
+    useEffect(() => {
+    }, [])
+    return (
+        <>
+        <div className='wrap'>
+               <div id="h1" ><h1>Weather App</h1></div>
+               <div>
+                    <input 
+                        type="search" placeholder='Enter the City Name...'
+                        autoFocus id="search"
+                        className='searchTeam'
+                        value={searchValue}
+                        onChange={(e)=>{setSearchValue(e.target.value)}}
+                    />
+                    <button className='searchButton' type='button' onClick={getWeatherInfo}>Search</button>
+                    </div>
+        </div>
+        <WeatherInfo tempInfo={tempInfo}/>
+            
+            
+        </>
+    )
+}
+
+export default Weather;
---
+++ src/component/WeatherInfo.js
@@ -0,0 +1,108 @@
+import React,{useState,useEffect} from 'react'
+import './style.css'
+
+const WeatherInfo = ({ tempInfo }) => {
+
+    const [weatherState, setWeatherState] = useState("");
+    let[getTime, setTime]=useState();
+
+    const {
+        temp,
+        humidity,
+        temp_max,
+        temp_min,
+        pressure,
+        name,
+        country,
+        sunset,
+        speed,
+        weatherMood
+    } = tempInfo;
+    
+    let sec = sunset;
+    let date = new Date(sec * 1000);
+    let time = `${date.getHours()}:${date.getMinutes()}`;
+
+    const Set =() => {
+        var realTime=new Date().toLocaleString();
+        setTime(realTime);
+    }
+
+    setInterval(Set,1000);
+    
+    useEffect(() => {
+        if (weatherMood) {
+            switch (weatherMood) {
+                case "Clouds":
+                    setWeatherState("wi-day-cloudy");
+                    break;
+                case "Haze":
+                    setWeatherState("wi-fog");
+                    break;
+                case "Clear":
+                    setWeatherState("wi-day-sunny");
+                    break;
+                case "Mist":
+                    setWeatherState("wi-dust");
+                    break;
+
+                default:
+                    setWeatherState("wi-day-sunny");
+                    break;
+            }
+        }
+    }, [weatherMood]);
+
+    return (
+        <>
+            <div className='widget'>
+
+                <div className='weatherIcon'>
+                    <i id="icon" className={`wi ${weatherState}`}></i>
+                </div>
+
+                <div className='weatherInfo'>
+                    <div className='temperature'>
+                        <span>{temp}&deg;</span>
+                    </div>
+
+                    <div className='description'>
+                        <div className='weatherCondition'>{weatherMood}</div>
+                        <div className='place'>{name} {country}</div>
+                    </div>
+
+                    <div className='date'>
+                        {getTime}
+                    </div>
+                </div>
+
+                <div className='info'>
+
+                    <div id="sunset_secton" className='two-sided-section'>
+                        <p><i id="sunset" className={"wi wi-sunset"}></i></p>
+                        <p className='extra-info-leftside'>Sunset<br />{time} </p>
+                    </div>
+
+                    <div className='two-sided-section'>
+                        <p><i id="humidity" className={"wi wi-humidity"}></i></p>
+                        <p className='extra-info-leftside'>Humidity<br /> {humidity} </p>
+                    </div>
+
+
+                    <div className='two-sided-section'>
+                        <p><i id="pressure" className={"wi wi-rain"}></i></p>
+                        <p className='extra-info-leftside'>Pressure<br />{pressure} </p>
+                    </div>
+
+                    <div id="wind_secton" className='two-sided-section'>
+                        <i id="speed" className={"wi wi-strong-wind"}></i>
+                        <p className='extra-info-leftside'>Wind<br/>{speed}</p>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WeatherInfo
